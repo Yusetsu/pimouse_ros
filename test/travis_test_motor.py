2fbd60864fabaf806e81403ac3d7c14706f543c6
Yusetsu <yusetsu2@hotmail.co.jp>
2019-04-11 15:53:12 +0000

    test8

--- test/travis_test_motor.py
+++ test/travis_test_motor.py
@@ -18,8 +18,8 @@ class MotorTest(unittest.TestCase):
 	def test_put_freq(self):
 		pub = rospy.Publisher('/motors_raw',MotorFreqs)
 		m = MotorFreqs()
-		m.left_hz=123
-		m.right_hz=456
+		m.left_hz = 123
+		m.right_hz = 456
 		for i in range(10):
 			pub.publish(m)
 			time.sleep(0.1)
@@ -28,10 +28,10 @@ class MotorTest(unittest.TestCase):
 		self.file_check("rtmotor_raw_r0",m.right_hz,"wrong left value from motor_raw")
 
 	def test_put_cmd_vel(self):
-		pub=rospy.Publisher('/cmd_vel',Twist)
-		m=Twist()
-		m.linear.x=0.1414
-		m.angular.z=1.57
+		pub = rospy.Publisher('/cmd_vel',Twist)
+		m = Twist()
+		m.linear.x = 0.1414
+		m.angular.z = 1.57
 		for i in range(10):
 			pub.publish(m)
 			time.sleep(0.1)
